0af925b7787136c9054b3c72db8adb7c64022a06
Fasilafirthous <96758426+Fasilafirthous@users.noreply.github.com>
2024-09-29 12:12:39 +0530

    Delete BE/blog-project directory

--- BE/blog-project/src/module/blog/blog.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { BlogService } from './blog.service';
-import { BlogResolver } from './blog.resolver';
-import { BlogRepository } from './blog.repository';
-
-@Module({
-  providers: [BlogResolver, BlogService,BlogRepository]
-})
-export class BlogModule {}
